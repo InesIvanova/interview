64de8a782ff81265ed1696839130c83a0218e1ac
InesIvanova <ines.iv.ivanova@gmail.com>
2019-09-13 02:09:56 +0300

    FIXED step 1 completed. Adding status codes and error handling for unique username

--- views.py
+++ views.py
@@ -1,7 +1,10 @@
 from flask_restful import Resource, reqparse, request
 from flask import jsonify, request
+from sqlalchemy.exc import SQLAlchemyError
+
 from models import Contact
 from app import db
+from flask_api import status, exceptions
 import json
 
 parser = reqparse.RequestParser()
@@ -13,46 +16,55 @@ class ContactListCreateView(Resource):
             username = request.args.to_dict()['username']
             contact = Contact.query.filter_by(username=username).first()
             if contact:
-                return jsonify(contact.serialize)
-            return jsonify({'error': 'no such username'})
+                return jsonify({'contact': contact.serialize, 'message': 'OK', 'status': status.HTTP_200_OK})
+            return jsonify({'message': 'Not Found', 'status': status.HTTP_404_NOT_FOUND})
         contacts = Contact.query.all()
         return jsonify({'contacts': [result.serialize for result in contacts]})
 
     def post(self):
         if request.form:
-            #TODO get the data in specific format from form and
-            # replace in contact and include error handling
             data = request.form
-            contact = Contact(username='noraa2', first_name='Nora', last_name='Kenova')
+            username = data['username']
+            first_name = data['first_name']
+            last_name = data['last_name']
+            contact = Contact(username=username, first_name=first_name, last_name=last_name)
             db.session.add(contact)
-            db.session.commit()
+
+            try:
+                db.session.commit()
+            except SQLAlchemyError as ex:
+                error = str(ex.__dict__['orig'])
+                return jsonify({'message': error, 'status': status.HTTP_400_BAD_REQUEST})
+
             return jsonify({'status': '201'})
-        else:
-            print('noo')
-            return jsonify({'error': 'error'})
+        return jsonify({'message': 'Request data was not in a correct format', 'status': status.HTTP_400_BAD_REQUEST})
 
 
 class ContactRetrieveUpdateDeleteView(Resource):
     def get(self, id):
         contact = Contact.query.filter_by(id=id).first()
         if contact:
-            return jsonify(contact.serialize)
-        return jsonify({'error': 'no such id'})
+            return jsonify({'contact': contact.serialize, 'message': 'OK', 'status': status.HTTP_200_OK})
+        return jsonify({'message': 'Not Found', 'status': status.HTTP_404_NOT_FOUND})
 
-    def update(self, id):
+    def put(self, id):
         contact = Contact.query.filter_by(id=id).first()
         if contact:
-            #TODO get this from form (as in post request) replace value and save it to db
-            db.session.update()
-            return jsonify(contact.serialize)
-        return jsonify({'error': 'no such id'})
+            data = request.form
+            contact.username = data['username']
+            contact.first_name = data['first_name']
+            contact.last_name = data['last_name']
+            db.session.commit()
+            return jsonify({'message': 'Resource Updated Successfully', 'status': status.HTTP_200_OK})
+        return jsonify({'message': 'Not Found', 'status': status.HTTP_404_NOT_FOUND})
 
     def delete(self, id):
         contact = Contact.query.filter_by(id=id).first()
         if contact:
             db.session.delete(contact)
             db.session.commit()
-            return jsonify({'delete': 'ok'})
-        return jsonify({'error': 'no such id'})
+            return jsonify({'message': 'No Content', 'status': status.HTTP_204_NO_CONTENT})
+        return jsonify({'message': 'Not Found', 'status': status.HTTP_404_NOT_FOUND})
+
 
 
